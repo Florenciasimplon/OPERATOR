8bd86a845df8911393a2f2a2f27183664ecdab35
Florenciasimplon <florenciasimplon@gmail.com>
2021-05-07 13:55:19 +0200

    MAJ

--- htdocs/index.php
+++ htdocs/index.php
@@ -13,11 +13,8 @@ $ReviewManager = new ReviewManager($pdo);
 include 'data-recovery/poc-promo.php';
 include 'forms/search.php';
 include 'data-recovery/destinations.php';
-
+include 'data-recovery/gallery.php';
 ?>
-<h1>test</h1>
-<i class='fa fa-star' style='color:yellow; border:black;'></i>
-<i class='far fa-star'></i>
-<i class="fas fa-camera"></i> 
+
 </body>
 <?php include 'partiels/footer.php'; ?>
